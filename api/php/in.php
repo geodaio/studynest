3f5df927b5616f1f6cd754c7910b54566ac31d3d
geodaio <powheart26@gmail.com>
2023-11-30 12:34:38 -0500

    Update in.php

--- api/php/in.php
+++ api/php/in.php
@@ -2,14 +2,20 @@
   if(isset($_POST['submit'])) {
     include($_SERVER["DOCUMENT_ROOT"] . "connect.js");
 
-    $email = $_POST("email");
-    $password = $_POST("password");
+    $email = trim($_POST("email"));
+    $password = trim($_POST("password"));
 
-    if (){
-
+    pool.query("SELECT * FROM accounts WHERE email = \'" . $email ."\';");
+    $rowcount = pool.query("SELECT @@ROWCOUNT");
+    if ($rowcount > 0){
+      if (pool.query("SELECT userpassword FROM accounts WHERE email = \'" . $email . "\';") == $password){
+        pool.query("UPDATE accounts SET online = true WHERE email = \'" . $email . "\';");
+        include($_SERVER["DOCUMENT_ROOT"] . "logInOut.js");
+        pool.end();
+      }
     }
     else {
-
+      echo("<p>No account found for this email!</p>);
     }
   }
 ?>
