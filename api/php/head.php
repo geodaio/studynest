e7b81889975112b7c01d02e974e1650648da2fcd
geodaio <powheart26@gmail.com>
2023-11-30 12:37:51 -0500

    Update head.php

--- api/php/head.php
+++ api/php/head.php
@@ -4,16 +4,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     
-    <title>
     <?php
       echo $title;
     ?>
-    </title>
     <link rel="shortcut icon" type="image/x-icon" href="/api/images/favicon.ico" id="favicon"/>
     
     <link rel="stylesheet" type="text/css" href="/api/css/style.php">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=PT+Serif&display=swap" rel="stylesheet"> <!--Previous three links are links for Google Fonts-->
+    <script src="https://kit.fontawesome.com/b32b8e4e05.js" crossorigin="anonymous"></script>
 </head>
