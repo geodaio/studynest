dee8b7455af451d0e32559da0d3937366d5fe736
geodaio <powheart26@gmail.com>
2023-11-30 09:09:23 -0500

    Create nav-in.php

---
+++ api/php/nav-in.php
@@ -0,0 +1,10 @@
+<button class="notification-bell" id="notifications-header"><img src="api/images/notification_logo.svg" alt="ALTTEXT" id="notification-bell-img"></button>
+<div class="dropdown">
+	<button class="dropdown-button" id="profile-header"><img src="/apiimages/profile.jpeg" alt="ALTTEXT" id="profile-pic-header"></button>
+	<div class="lang-options">
+		<a href="api/php/profile.php" title="" id="profile-drop-header">Profile</a>
+		<a href="api/php/messages.php" title="" id="messages-header">Messages</a>
+		<a href="api/php/settings.php" title="" id="settings-header" onclick="changeLang(this.id)">Settings</a>
+		<a href="" title="" id="log-out-header" onclick="signOut()">Log Out</a>
+	</div>
+</div>
