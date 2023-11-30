0207c24b5fe62e4d781ca399024e34cc13cc657d
geodaio <powheart26@gmail.com>
2023-11-30 09:09:03 -0500

    Create nav-left.php

---
+++ api/php/nav-left.php
@@ -0,0 +1,7 @@
+<div class="header-left">
+  <img src="api/images/SNLogo.svg" alt="Logo Temp" id="header-logo">
+  <a href="api/php/index.php" title="Temp">Home</a> 
+  <a href="api/php/hub.php" title="Temp">Study Hub</a>
+  <a href="api/php/communities.php" title="Temp">Communities</a>
+  <a href="api/php/about-us.php" title="Temp">About Us</a>
+</div>
