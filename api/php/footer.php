cf25bad230f40bdd462f4483dedead3ab74571f0
geodaio <powheart26@gmail.com>
2023-11-30 12:46:58 -0500

    Update footer.php

--- api/php/footer.php
+++ api/php/footer.php
@@ -1,27 +1,26 @@
-<footer>
-  <div class="content">
-    <div class="top">
-        <div class="logo-details">
-            <span class="logo_name"><img src="SNLogo.svg" alt="Logo Temp" id="header-logo2"></span>
+ <footer>
+        <div class="content">
+        <div class="top">
+            <div class="logo-details">
+            <span class="logo_name"><img src="images/footerlogo.svg" alt="Logo of Study nest in white" id="header-logo2"></span>
         </div>
-        <div class="media-icons">
-            <a href="#"><i class="fa-facebook"></i></a>
-            <a href="#"><i class="fa-instagram"></i></a>
-            <a href="#"><i class="fa-facebook"></i></a>
-            <a href="#"><i class="fa-facebook"></i></a>
-            <a href="#"><i class="fa-facebook"></i></a>
-        </div>
-    </div>
-    <hr class="hrstyle">
-	      <div class="bottom-details">
-	          <div class="bottom_text">
-	              <span class="copyright_text"><a href="#"> Study Nest© 2023. All rights reserved</a></span>
-	              <span class="Policy-terms">
-	                  <a href="#">Privacy policy</a>
-	                  <a href="#">Terms and condtions</a>
-	              </span>
-	          </div>
-	      </div>
-	  	<div class="link-boxes"></div>
+            <div class="media-icons">
+                <a href="#"><i class="fa-brands fa-square-facebook fa-xl"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram fa-xl"></i></a>
+                <a href="#"><i class="fa-brands fa-tiktok fa-xl"></i></a>
+                <a href="#"><i class="fa-brands fa-linkedin fa-xl"></i></a>
+            </div>
+          </div>
+            <hr class="hrstyle">
+            <div class="bottom-details">
+            <div class="bottom_text">
+                <span class="copyright_text"><a href="#"> Study Nest© 2023. All rights reserved</a></span>
+                <span class="Policy-terms">
+                    <a href="#">Privacy policy</a>
+                    <a href="#">Terms and condtions</a>
+                </span>
+            </div>
+            </div>
+          <div class="link-boxes"></div>
 	</div>
 </footer>
