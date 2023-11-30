661ca30f416343e7ca402d16c0b70d7652905c43
geodaio <powheart26@gmail.com>
2023-11-30 09:12:50 -0500

    Create settings.php

---
+++ api/php/settings.php
@@ -0,0 +1,14 @@
+<?php
+    $title = "Settings";
+    include("api/php/head.php");
+?>
+    <body class="homepage">
+        <?php
+            include("api/php/nav.php");
+        ?>
+        <?php
+            include("api/php/footer.php");
+            include("api/php/scripts.php");
+        ?>
+    </body>
+</html>
