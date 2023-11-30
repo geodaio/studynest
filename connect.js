e4d486515dcf694f2d78561d3e9068f610577802
geodaio <powheart26@gmail.com>
2023-11-30 09:46:01 -0500

    Create connect.js

---
+++ connect.js
@@ -0,0 +1,27 @@
+//https://www.cockroachlabs.com/blog/full-stack-node-app/
+
+const express = require('express');
+const app = express();
+
+const Sequelize = require("sequelize-cockroachdb");
+
+const fs = require("fs");
+
+var sequelize = new Sequelize({
+  dialect: "postgres",
+  username: "geodaio",
+  password: env.PASSWORD,
+  host: "study-nest-12836.7tt.cockroachlabs.cloud",
+  port: 26257,
+  database: "defaultdb",
+  dialectOptions: {
+    ssl: {
+      ca: fs.readFileSync(env.CERT).toString();
+    },
+  },
+  logging: false,
+});
+
+app.listen(port, host, () =>{
+  console.log("Server started at ${host} port ${post}");
+})
