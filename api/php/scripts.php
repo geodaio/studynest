353ba16cb9aa18c0354dd4e439e3cabf68d47557
geodaio <powheart26@gmail.com>
2023-11-30 09:10:01 -0500

    Create scripts.php

---
+++ api/php/scripts.php
@@ -0,0 +1,3 @@
+<script type="text/javascript" src="api/javascript/home.js"></script>
+<script type="text/javascript" src="api/javascript/favicon.js"></script>
+<script type="text/javascript" src="api/javascript/logInOut.js"></script>
