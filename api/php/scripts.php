c302b657c433d132c6be3a3bf6fcb41c74d37090
geodaio <powheart26@gmail.com>
2023-11-30 12:48:10 -0500

    Update scripts.php

--- api/php/scripts.php
+++ api/php/scripts.php
@@ -1,3 +1,5 @@
 <script type="text/javascript" src="api/javascript/home.js"></script>
 <script type="text/javascript" src="api/javascript/favicon.js"></script>
 <script type="text/javascript" src="api/javascript/logInOut.js"></script>
+<script type="text/javascript" src="api/javascript/studynest.js"></script>
+<script type="text/javascript" src="api/javascript/profile.js"></script>
