23debadd40f63ef46b3bd7b1308e09539367e8df
geodaio <powheart26@gmail.com>
2023-11-30 11:21:45 -0500

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 const { Pool, Client } = require("pg");
 
-const connectionString = "postgresql://geodaio"+env.PASSWORD+"study-nest-12836.7tt.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&sslrootcert="+env.CERT+"&options=--cluster%3Dstudy-nest";
+const connectionString = "postgresql://geodaio"+${{ env.PASSWORD }}+"study-nest-12836.7tt.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&sslrootcert="+${{ env.CERT }}+"&options=--cluster%3Dstudy-nest";
 
 const pool = new Pool ({connectionString});
 
