9b343d28e52c53eb11a25872462eae213a536b30
geodaio <powheart26@gmail.com>
2023-11-30 09:12:29 -0500

    Create about-us.php

---
+++ api/php/about-us.php
@@ -0,0 +1,14 @@
+<?php
+    $title = "About Us";
+    include("api/php/head.php");
+?>
+    <body class="homepage">
+        <?php
+            include("api/php/nav.php");
+        ?>
+        <?php
+            include("api/php/footer.php");
+            include("api/php/scripts.php");
+        ?>
+    </body>
+</html>
