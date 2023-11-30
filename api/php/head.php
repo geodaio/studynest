09d46929fe231fb19b2bf7494c532f253b070a95
geodaio <powheart26@gmail.com>
2023-11-30 09:10:50 -0500

    Create head.php

---
+++ api/php/head.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    
+    <title>
+    <?php
+      echo $title;
+    ?>
+    </title>
+    <link rel="shortcut icon" type="image/x-icon" href="/api/images/favicon.ico" id="favicon"/>
+    
+    <link rel="stylesheet" type="text/css" href="/api/css/style.php">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=PT+Serif&display=swap" rel="stylesheet"> <!--Previous three links are links for Google Fonts-->
+</head>
