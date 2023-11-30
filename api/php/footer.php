e8507bbe5b08b1e250453d4fda67b3da9d4c1818
geodaio <powheart26@gmail.com>
2023-11-30 09:11:05 -0500

    Create footer.php

---
+++ api/php/footer.php
@@ -0,0 +1,27 @@
+<footer>
+  <div class="content">
+    <div class="top">
+        <div class="logo-details">
+            <span class="logo_name"><img src="SNLogo.svg" alt="Logo Temp" id="header-logo2"></span>
+        </div>
+        <div class="media-icons">
+            <a href="#"><i class="fa-facebook"></i></a>
+            <a href="#"><i class="fa-instagram"></i></a>
+            <a href="#"><i class="fa-facebook"></i></a>
+            <a href="#"><i class="fa-facebook"></i></a>
+            <a href="#"><i class="fa-facebook"></i></a>
+        </div>
+    </div>
+    <hr class="hrstyle">
+	      <div class="bottom-details">
+	          <div class="bottom_text">
+	              <span class="copyright_text"><a href="#"> Study Nest© 2023. All rights reserved</a></span>
+	              <span class="Policy-terms">
+	                  <a href="#">Privacy policy</a>
+	                  <a href="#">Terms and condtions</a>
+	              </span>
+	          </div>
+	      </div>
+	  	<div class="link-boxes"></div>
+	</div>
+</footer>
