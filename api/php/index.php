9e021102fceaa4aaeb4a7cce2bc83c530ae54066
geodaio <powheart26@gmail.com>
2023-11-30 12:48:58 -0500

    Update index.php

--- api/php/index.php
+++ api/php/index.php
@@ -147,39 +147,9 @@
                   <button id="join-newsletter">Subscribe</button></div>
             </section>
         </div>
-    
-        <footer>
-            
-        </footer>
-        <script src="js/studynest.js"></script>
-        <script src="js/favicon.js"></script>
-        <script src="js/logInOut.js"></script>
- <footer>
-        <div class="content">
-        <div class="top">
-            <div class="logo-details">
-            <span class="logo_name"><img src="images/footerlogo.svg" alt="Logo of Study nest in white" id="header-logo2"></span>
-        </div>
-            <div class="media-icons">
-                <a href="#"><i class="fa-brands fa-square-facebook fa-xl"></i></a>
-                <a href="#"><i class="fa-brands fa-instagram fa-xl"></i></a>
-                <a href="#"><i class="fa-brands fa-tiktok fa-xl"></i></a>
-                <a href="#"><i class="fa-brands fa-linkedin fa-xl"></i></a>
-            </div>
-          </div>
-            <hr class="hrstyle">
-            <div class="bottom-details">
-            <div class="bottom_text">
-                <span class="copyright_text"><a href="#"> Study Nest© 2023. All rights reserved</a></span>
-                <span class="Policy-terms">
-                    <a href="#">Privacy policy</a>
-                    <a href="#">Terms and condtions</a>
-                </span>
-            </div>
-            </div>
-          <div class="link-boxes"></div>
-        </div>
-        </footer>
-<script src="js/studynest.js"></script>
- </body>
+        <?php
+            include("$_SERVER[DOCUMENT_ROOT]/api/php/footer.php");
+            include("$_SERVER[DOCUMENT_ROOT]/api/php/scripts.php");
+        ?>
+    </body>
 </html>
