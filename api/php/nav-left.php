b30ad550acb7765bd01775e2afab5d855c2d552f
geodaio <powheart26@gmail.com>
2023-11-30 12:43:57 -0500

    Update nav-left.php

--- api/php/nav-left.php
+++ api/php/nav-left.php
@@ -1,7 +1,29 @@
 <div class="header-left">
-  <img src="api/images/SNLogo.svg" alt="Logo Temp" id="header-logo">
-  <a href="api/php/index.php" title="Temp">Home</a> 
-  <a href="api/php/hub.php" title="Temp">Study Hub</a>
-  <a href="api/php/communities.php" title="Temp">Communities</a>
-  <a href="api/php/about-us.php" title="Temp">About Us</a>
-</div>
+            <div class="brand-title">
+              <a href="index.html"><img src="images/SNLogo.svg" alt="Logo that says StudyNest" id="header-logo"></a>
+            </div>
+            <a href="#" class="toggle-button" onclick="openNav()">
+              <span class="bar"></span>
+              <span class="bar"></span>
+              <span class="bar"></span>
+            </a>
+             <div class="navbar-links">
+              <ul>
+                <li><a href="hub.html" title="Study Hub">Study Hub</a></li> 
+                <li><a href="" title="Temp">How it Works</a></li>
+                <li><a href="" title="Temp">Resources</a></li>
+                <li><a href="" title="Temp">About Us</a></li>
+              </ul>
+            </div>
+            <div id="mySidenav" class="sidenav">
+                <div class="topheader">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                    <a href="hub.html" title="Study Hub">Study Hub</a>
+                    <a href="#">How it Works</a>
+                    <a href="#">Resources</a>
+                    <a href="#">About Us</a>
+                    <button class="signin2"  onclick="toLogin()">Sign In</button>
+                    <button class="signup2" onclick="">Sign Up</button>
+                </div>
+            </div>
+        </div>
