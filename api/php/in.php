08b03547a946a9f01d4b30ea6061fcdffaf33ac8
geodaio <powheart26@gmail.com>
2023-12-03 16:20:56 -0500

    Update in.php

--- api/php/in.php
+++ api/php/in.php
@@ -3,13 +3,18 @@
 
     $email = trim($_POST("email"));
     $password = trim($_POST("password"));
-
+    
+    echo $email;
+    echo $password;
+    
     include($_SERVER('DOCUMENT_ROOT') . "connect.php");
 
     $sql = 'SELECT email FROM accounts WHERE email = \'' . $email . '\';';
 
     $rowcount = $connec->exec($sql);
 
+    echo $rowcount;
+    
     if($rowcount != 0){
       $sql = 'SELECT password FROM accounts WHERE email =\'' . $email . '\';';
       $passwordOnFile = $connec->query($sql);
@@ -19,11 +24,11 @@
         signIn();
       }
       else {
-        echo("<p>Incorrect password for this email address. Please enter the password associated with this account, or reset your password.</p>");
+        echo "<p>Incorrect password for this email address. Please enter the password associated with this account, or reset your password.</p>";
       }
     }
     else {
-      echo("<p>No account attatched to this email. Please enter a valid email address, or sign up now!</p>");
+      echo "<p>No account attatched to this email. Please enter a valid email address, or sign up now!</p>";
     }
   }
 ?>
