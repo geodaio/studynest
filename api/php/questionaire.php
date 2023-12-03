9a8e462f4c26ac19450f0e91b800b2fd90992999
geodaio <powheart26@gmail.com>
2023-12-03 16:07:39 -0500

    Update questionaire.php

--- api/php/questionaire.php
+++ api/php/questionaire.php
@@ -1,26 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    
-    <title>Home</title>
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" id="favicon"/>
-    
-    <link rel="stylesheet" type="text/css" href="css/studynest.css">
 
+<?php
+    $title = "Home";
+    include($_SERVER["DOCUMENT_ROOT"] . "/api/php/head.php");
+?>
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <script src="https://kit.fontawesome.com/b32b8e4e05.js" crossorigin="anonymous"></script>
-      <style>  
-     
-
-
-    </style>
-
-</head>
     <body class="Query">
 
 <div class="containerbox">
