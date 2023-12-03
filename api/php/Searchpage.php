6e578d054e174482a93e9f0dc807c18de02f3e73
geodaio <powheart26@gmail.com>
2023-12-03 14:59:26 -0500

    Update Searchpage.php

--- api/php/Searchpage.php
+++ api/php/Searchpage.php
@@ -1,76 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Study Hub</title>
-	<link rel="stylesheet" type="text/css" href="css/studynest.css">
-   <link href="https://fonts.cdnfonts.com/css/nimbus-sans-l" rel="stylesheet">
-   <link href='https://fonts.googleapis.com/css?family=Lora' rel='stylesheet'>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
+<?php
+    $title = "About Us";
+    include("api/php/head.php");
+?>
 
-  <body class="searching">
-<div class="searchcontainer">
+<body class="searching">
+	<div class="searchcontainer">
       <div class="top">
-       
-<header id="header-out">
-      <nav class="navbar flexbox ">
-         <div class="header-left">
-        <div class="brand-title">
-          <a href="index.html"><img src="images/SNLogo.svg" alt="Logo Temp" id="header-logo"></a>
-        </div>
-        <a href="#" class="toggle-button" onclick="openNav()">
-          <span class="bar"></span>
-          <span class="bar"></span>
-          <span class="bar"></span>
-        </a>
-     <div class="navbar-links">
-          <ul>
-       <li><a href="hub.html" title="Study Hub">Study Hub</a></li> 
-            <li><a href="" title="Temp">How it Works</a></li>
-            <li><a href="" title="Temp">Resources</a></li>
-            <li><a href="" title="Temp">About Us</a></li>
-          </ul>
-        </div>
-           
-           <div id="mySidenav" class="sidenav">
-             <div class="topheader">
-              <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-              <a href="#">How it Works</a>
-              <a href="#">Resources</a>
-              <a href="#">About Us</a>
-             <button class="signin2" onclick="toLogin()">Sign In</button>
-             <button class="signup2" onclick="">Sign Up</button>
-             
-           </div>
-</div>
-        </div>
-        
-        <div class="header-right">
-          
-                    <div class="dropdown">
-                        <button class="dropdown-button" id="lang"><img src="images/Globe_icon.svg" alt="ALTTEXT" id="dropdown-img">English</button>
-                        <div class="lang-options">
-                            <a href="" title="English" id="English" onclick="changeLang(this.id)">English</a>
-                            <a href="" title="Spanish" id="Spanish" onclick="changeLang(this.id)">Spanish</a>
-                            <a href="" title="French" id="French" onclick="changeLang(this.id)">French</a>
-                            <a href="" title="German" id="German" onclick="changeLang(this.id)">German</a>
-                            <a href="" title="Russian" id="Russian" onclick="changeLang(this.id)">Russian</a>
-                        </div>
-                    </div>
-                    <button class="signin" id="signin" onclick="toLogin()">Sign In</button>
-                      <button class="signup" onclick="">Sign Up</button>
-                </div>
- 
-      </nav>
-      </header>    
-    
-       <!-- Insert nav here abi -->
+       	<?php
+        	include($_SERVER["DOCUMENT_ROOT"] . "/api/php/nav.php");
+   		?>   
       </div>
-<div class="searchcontent">
-  <div class="lefthub">
-         <a href="hub.html" class="backbtn"><i class="fa fa-arrow-right fa-rotate-180" style="color: #29382f;"></i>Back to Study Hub</a>
+	<div class="searchcontent">
+  		<div class="lefthub">
+         	<a href="hub.html" class="backbtn"><i class="fa fa-arrow-right fa-rotate-180" style="color: #29382f;"></i>Back to Study Hub</a>
     <div class= something>
       <div class="subsection">
         <h2 class="searchsubtitle">Possible Study Buddies</h2>
@@ -171,12 +114,16 @@
         </div>
         <button class="testingsome">Message</button>
         </div></div>
-    </div>
-        <div class="righthub">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2932.8825009416455!2d-73.82736522427992!3d42.68503121450953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89de0b3b15c5cb35%3A0xc7c3edbfb0809e9e!2sUniversity%20at%20Albany!5e0!3m2!1sen!2sus!4v1701107071580!5m2!1sen!2sus" style="border:0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="mapmoment"></iframe>
-        </div>
-</div>
-</div>
+    	</div>
+        	<div class="righthub">
+            	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2932.8825009416455!2d-73.82736522427992!3d42.68503121450953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89de0b3b15c5cb35%3A0xc7c3edbfb0809e9e!2sUniversity%20at%20Albany!5e0!3m2!1sen!2sus!4v1701107071580!5m2!1sen!2sus" style="border:0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="mapmoment"></iframe>
+        	</div>
+		</div>
+	</div>
 
+	<?php
+        include("$_SERVER[DOCUMENT_ROOT]/api/php/footer.php");
+        include("$_SERVER[DOCUMENT_ROOT]/api/php/scripts.php");
+    ?>
 </body>
 </html>
