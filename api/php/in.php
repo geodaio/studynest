1a587564aeacda5a7b048e0025dbf9a5801e3509
geodaio <powheart26@gmail.com>
2023-12-03 13:57:13 -0500

    Update in.php

--- api/php/in.php
+++ api/php/in.php
@@ -4,5 +4,10 @@
     $email = trim($_POST("email"));
     $password = trim($_POST("password"));
 
+    include($_SERVER('DOCUMENT_ROOT') . "connect.php");
+
+    $sql = '';
+
+    foreach()
   }
 ?>
