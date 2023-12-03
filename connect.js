7b20057db8ae6c664c045f3bc4cb922a64f0135c
geodaio <powheart26@gmail.com>
2023-12-02 20:25:21 -0500

    Delete connect.js

--- connect.js
+++
@@ -1,7 +0,0 @@
-const { Pool, Client } = require("pg");
-
-const connectionString = "postgresql://geodaio"+${{ secrets.PASSWORD }}+"study-nest-12836.7tt.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&sslrootcert="+${{ secrets.CERT }}+"&options=--cluster%3Dstudy-nest";
-
-const pool = new Pool ({connectionString});
-
-
