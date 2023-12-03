0e2a12944d11dc2611258418e50ef4988a9f4094
geodaio <powheart26@gmail.com>
2023-12-03 16:02:36 -0500

    Update in.php

--- api/php/in.php
+++ api/php/in.php
@@ -6,8 +6,24 @@
 
     include($_SERVER('DOCUMENT_ROOT') . "connect.php");
 
-    $sql = '';
+    $sql = 'SELECT email FROM accounts WHERE email = \'' . $email . '\';';
 
-    foreach()
+    $rowcount = $connec->exec($sql);
+
+    if($rowcount != 0){
+      $sql = 'SELECT password FROM accounts WHERE email =\'' . $email . '\';';
+      $passwordOnFile = $connec->query($sql);
+
+      if (strcmp(string $passwordOnFile, string $password) == 0){
+        include("logInOut.js");
+        signIn();
+      }
+      else {
+        echo("<p>Incorrect password for this email address. Please enter the password associated with this account, or reset your password.</p>");
+      }
+    }
+    else {
+      echo("<p>No account attatched to this email. Please enter a valid email address, or sign up now!</p>");
+    }
   }
 ?>
