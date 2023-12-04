35c691b1cff8de3f21f47a30881d558e7c60807b
geodaio <powheart26@gmail.com>
2023-12-03 21:05:50 -0500

    Create formhandler.js

---
+++ api/javascript/formhandler.js
@@ -0,0 +1,19 @@
+const form = document.getElementById("form-login");
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const formData = new FormData(form);
+
+  fetch("api/php/in.php", {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    if (response.ok) {
+      logInOut.signIn();
+    } 
+    else {
+      myDiv.innerHTML = '<p>There was an error completing your log in. Please try again later.</p>';
+    }
+  });
+});
