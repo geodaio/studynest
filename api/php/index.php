4ac65092fa46932bf82b5a51b23c52ea9a98e1aa
geodaio <powheart26@gmail.com>
2023-12-04 11:39:28 -0500

    Update index.php

--- api/php/index.php
+++ api/php/index.php
@@ -29,6 +29,7 @@
                 <div class="right">
                  <div class="row scrolling_text">
                         <div class="runningtext">
+                        <img src="api/images/unicon.php" alt="Graduation Cap">
                         <span>Study Together</span>
                         <img src="api/images/unicon.php" alt="Graduation Cap">
                         <span>Study Together</span>
@@ -48,7 +49,6 @@
                          <img src="api/images/unicon.php" alt="Graduation Cap">
                         <span>Study Together</span>
                          <img src="api/images/unicon.php" alt="Graduation Cap">
-                        <span>Study Together</span>
                         </div>
                     </div>
                     </div>
