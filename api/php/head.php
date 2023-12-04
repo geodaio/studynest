587621b2de9ca761a7df9ac8393e4288f3cc51dc
geodaio <powheart26@gmail.com>
2023-12-03 20:24:00 -0500

    Update head.php

--- api/php/head.php
+++ api/php/head.php
@@ -3,10 +3,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
+
+    <title>
+        <?php
+          echo $title;
+        ?>
+    </title>
     
-    <?php
-      echo $title;
-    ?>
     <link rel="shortcut icon" type="image/x-icon" href="/api/images/favicon.ico" id="favicon"/>
     
     <link rel="stylesheet" type="text/css" href="/api/css/style.php">
