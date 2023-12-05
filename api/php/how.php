26567dead47bb3d58f38039544b474bbe5a2edf5
geodaio <powheart26@gmail.com>
2023-12-05 13:27:32 -0500

    Update how.php

--- api/php/how.php
+++ api/php/how.php
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
 <?php
     $title = "How We Work";
-    include("api/php/head.php");
+    include($_SERVER["DOCUMENT_ROOT"] . "/api/php/head.php");
+
 ?>
     <body class="homepage">
         <?php
-            include("api/php/nav.php");
+            include($_SERVER["DOCUMENT_ROOT"] . "/api/php/nav.php");
         ?>
+        <h1>So, how does Study Nest Work?</h1>
+        
+        <h2>Firstly, you sign up for an account.</h2>
+        <p>test</p>
+        
+        <h2>Once you have signed up for an account, you will receive buddies similar to yourself.</h2>
+        <p>test</p>
+        
+        <h2>More.</h2>
+        <p>test</p>
+        
+        <h2>More.</h2>
+        <p>test</p>
+        
+        <h2>More.</h2>
+        <p>test</p>
+        
+        <h2>More.</h2>
+        <p>test</p>
         <?php
-            include("api/php/footer.php");
-            include("api/php/scripts.php");
+            include($_SERVER["DOCUMENT_ROOT"] . "/api/php/footer.php");
+            include($_SERVER["DOCUMENT_ROOT"] . "/api/php/scripts.php");
         ?>
     </body>
 </html>
