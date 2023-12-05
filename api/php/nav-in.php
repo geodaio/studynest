1129be1f7d6d82e3a87c5e6f1d26a708723c9e89
geodaio <powheart26@gmail.com>
2023-12-05 14:19:40 -0500

    Update nav-in.php

--- api/php/nav-in.php
+++ api/php/nav-in.php
@@ -1,6 +1,6 @@
 <button class="notification-bell" id="notifications-header"><img src="api/images/notification_logo.svg" alt="ALTTEXT" id="notification-bell-img"></button>
 <div class="dropdown">
-	<button class="dropdown-button" id="profile-header"><img src="/apiimages/profile.jpeg" alt="ALTTEXT" id="profile-pic-header"></button>
+	<button class="dropdown-button" id="profile-header"><img src="api/images/profile.jpeg" alt="ALTTEXT" id="profile-pic-header"></button>
 	<div class="lang-options">
 		<a href="api/php/profile.php" title="" id="profile-drop-header">Profile</a>
 		<a href="api/php/messages.php" title="" id="messages-header">Messages</a>
