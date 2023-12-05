4f5aefbeea5003b147a15afe3234bccd26549082
geodaio <powheart26@gmail.com>
2023-12-05 13:13:01 -0500

    Update about-us.php

--- api/php/about-us.php
+++ api/php/about-us.php
@@ -7,6 +7,26 @@
         <?php
             include("api/php/nav.php");
         ?>
+        <h1>Welcome to Study Nest</h1>
+        
+        <h2>What is Study Nest?</h2>
+        <p>test</p>
+        
+        <h2>How did Study Nest Come to Be?</h2>
+        <p>test</p>
+        
+        <h2>Who created Study Nest?</h2>
+        <p>test</p>
+        
+        <h2>How does Sudy Nest Work?</h2>
+        <h3>Local</h3>
+        <p>test</p>
+        <h3>Global</h3>
+        <p>test</p>
+
+        <h3>So, what are you waiting for? Sign up for Study Nest today!</h3>
+        <button type="button" id="">Sign Up Today!</button>
+        
         <?php
             include("api/php/footer.php");
             include("api/php/scripts.php");
