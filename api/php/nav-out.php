cbb3e792dcc31eab812db5fae92b2361e24451b5
geodaio <powheart26@gmail.com>
2023-12-07 13:49:18 -0500

    Update nav-out.php

--- api/php/nav-out.php
+++ api/php/nav-out.php
@@ -1,2 +1,4 @@
-<button class="sign-in" id="sign-in" onclick="toLogin()">Sign In</button>
-<button class="sign-up" onclick="toQNA()">Sign Up</button>
+<div class="navbar-links">
+  <button class="sign-in" id="sign-in" onclick="toLogin()">Sign In</button>
+  <button class="sign-up" onclick="toQNA()">Sign Up</button>
+</div>
