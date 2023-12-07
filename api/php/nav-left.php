f764915bbc5d617aba828eb57bf9a9387c1e6d3f
geodaio <powheart26@gmail.com>
2023-12-07 14:20:53 -0500

    Update nav-left.php

--- api/php/nav-left.php
+++ api/php/nav-left.php
@@ -15,25 +15,4 @@
                 <li><a href="api/php/about-us.php" title="Temp">About Us</a></li>
               </ul>
             </div>
-            <div id="mySidenav" class="sidenav">
-                <div class="topheader">
-                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                    <a href="api/php/hub.php" title="Study Hub">Study Hub</a>
-                    <a href="api/php/how.php">How it Works</a>
-                    <a href="api/php/resources.php">Resources</a>
-                    <a href="api/php/about-us.php">About Us</a>
-	        <?php
-                       	 if (isset($_COOKIE['loggedIn']) && $_COOKIE['loggedIn'] === 'true'){
-                                echo '<div class="navbar-links">';
-                                include($_SERVER['DOCUMENT_ROOT'] . "/api/php/nav-in.php");
-                                echo '</div>';
-                        }
-                         else {
-                                echo '<div class="navbar-links">';
-                                include($_SERVER['DOCUMENT_ROOT'] . "/api/php/nav-out.php");
-                                echo '</div>';
-                        }
-                     ?>
-		</div>
-	    </div>
 </div>
