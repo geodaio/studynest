653efa0c2baeb63ae6d796b9816dc7943131e321
geodaio <powheart26@gmail.com>
2023-12-07 15:52:20 -0500

    Update nav-in.php

--- api/php/nav-in.php
+++ api/php/nav-in.php
@@ -1,4 +1,4 @@
-<a href="#"><i class="fa-regular fa-bell"></i></a>
+<a href="#" id="notification-bell"><i class="fa-regular fa-bell"></i></a>
 <ul>
   <li><a href="api/php/profile.php" title="" id="profile-drop-header">Profile</a></li>
   <li><a href="api/php/messages.php" title="" id="messages-header">Messages</a></li>
