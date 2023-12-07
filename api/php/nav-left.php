535b82a5d1439f1203af919d286be6abb51ce1a7
geodaio <powheart26@gmail.com>
2023-12-07 14:13:45 -0500

    Update nav-left.php

--- api/php/nav-left.php
+++ api/php/nav-left.php
@@ -23,11 +23,15 @@
                     <a href="api/php/resources.php">Resources</a>
                     <a href="api/php/about-us.php">About Us</a>
 	        <?php
-                       	if (isset($_COOKIE['loggedIn']) && $_COOKIE['loggedIn'] === 'true'){
-                        	include($_SERVER['DOCUMENT_ROOT'] . "/api/php/nav-in.php");
+                       	 if (isset($_COOKIE['loggedIn']) && $_COOKIE['loggedIn'] === 'true'){
+                                echo '<div class="navbar-links">';
+                                include($_SERVER['DOCUMENT_ROOT'] . "/api/php/nav-in.php");
+                                echo '</div>';
                         }
-                        else {
-                                    include($_SERVER['DOCUMENT_ROOT'] . "/api/php/nav-out.php");
+                         else {
+                                echo '<div class="navbar-links">';
+                                include($_SERVER['DOCUMENT_ROOT'] . "/api/php/nav-out.php");
+                                echo '</div>';
                         }
                      ?>
 		</div>
