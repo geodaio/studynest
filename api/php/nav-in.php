eff8dea5a777e171119aaf17247e1be6dc9c1b90
geodaio <powheart26@gmail.com>
2023-12-07 13:49:04 -0500

    Update nav-in.php

--- api/php/nav-in.php
+++ api/php/nav-in.php
@@ -1,6 +1,7 @@
-<a href="#"><i class="fa-regular fa-bell"></i></a>
-<a href="api/php/profile.php" title="" id="profile-drop-header">Profile</a>
-<a href="api/php/messages.php" title="" id="messages-header">Messages</a>
-<a href="api/php/settings.php" title="" id="settings-header">Settings</a>
-<a href="#" title="" id="log-out-header" onclick="signOut()">Log Out</a>
-
+<div class="navbar-links">
+  <a href="#"><i class="fa-regular fa-bell"></i></a>
+  <a href="api/php/profile.php" title="" id="profile-drop-header">Profile</a>
+  <a href="api/php/messages.php" title="" id="messages-header">Messages</a>
+  <a href="api/php/settings.php" title="" id="settings-header">Settings</a>
+  <a href="#" title="" id="log-out-header" onclick="signOut()">Log Out</a>
+</div>
