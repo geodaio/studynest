c09eba69d212990a98566c1543a97ce777cc3b82
geodaio <powheart26@gmail.com>
2023-12-07 13:10:47 -0500

    Update nav.php

--- api/php/nav.php
+++ api/php/nav.php
@@ -6,7 +6,7 @@
                 <div class="header-right">
                         <?php
                                 if (isset($_COOKIE['loggedIn']) && $_COOKIE['loggedIn'] === 'true'){
-                                        include($_SERVER['DOCUMENT_ROOT'] . "/api/php/nav-in.php");
+                                        include($_SERVER['DOCUMENT_ROOT'] . "/api/php/nav-in-mobile.php");
                                 }
                                 else {
                                         include($_SERVER['DOCUMENT_ROOT'] . "/api/php/nav-out.php");
