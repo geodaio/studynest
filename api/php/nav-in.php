2e9d6b377771ad355a090d620c6e5cb363ffde93
geodaio <powheart26@gmail.com>
2023-12-07 13:32:36 -0500

    Update nav-in.php

--- api/php/nav-in.php
+++ api/php/nav-in.php
@@ -1,10 +1,6 @@
 <a href="#"><i class="fa-regular fa-bell"></i></a>
-<div class="dropdown">
-	<button class="dropdown-button" id="profile-header"><img src="api/images/profile.jpeg" alt="ALTTEXT" id="profile-pic-header"></button>
-	<div class="lang-options">
-		<a href="api/php/profile.php" title="" id="profile-drop-header">Profile</a>
-		<a href="api/php/messages.php" title="" id="messages-header">Messages</a>
-		<a href="api/php/settings.php" title="" id="settings-header">Settings</a>
-		<a href="#" title="" id="log-out-header" onclick="signOut()">Log Out</a>
-	</div>
-</div>
+<a href="api/php/profile.php" title="" id="profile-drop-header">Profile</a>
+<a href="api/php/messages.php" title="" id="messages-header">Messages</a>
+<a href="api/php/settings.php" title="" id="settings-header">Settings</a>
+<a href="#" title="" id="log-out-header" onclick="signOut()">Log Out</a>
+
