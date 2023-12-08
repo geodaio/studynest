08aabd0a20fc4e1653f5a7ee515a60387e54a6c6
geodaio <powheart26@gmail.com>
2023-12-08 01:17:24 -0500

    Update about-us.php

--- api/php/about-us.php
+++ api/php/about-us.php
@@ -8,7 +8,7 @@
             include($_SERVER["DOCUMENT_ROOT"] . "/api/php/nav.php");
         ?>
         <div id="how-page">
-            <h1>Welcome to Study Nest</h1>
+            <h1 id="text-title">About Us</h1>
             
             <h2>What is Study Nest?</h2>
             <p>Etiam sodales maximus dui, vel tristique neque luctus et. Donec sem nunc, scelerisque ac varius hendrerit, rutrum in urna. Sed sit amet arcu finibus, vestibulum orci nec, volutpat metus. Nulla facilisi. Nulla ultricies facilisis feugiat. Ut sagittis neque ex, sed posuere purus condimentum vitae.</p>
@@ -20,13 +20,7 @@
             <p>Suspendisse at nunc fermentum, consectetur dui non, mattis elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque et consequat arcu. Duis hendrerit faucibus vehicula. Vivamus iaculis nibh eget velit malesuada semper.</p>
             
             <h2>How does Study Nest Work?</h2>
-            <h3>Local</h3>
-            <p>Maecenas iaculis nisi sed augue condimentum vehicula. Nunc commodo nibh in elit accumsan molestie.</p>
-            <h3>Global</h3>
-            <p>Donec bibendum in dui quis vehicula. Aenean ultrices pulvinar diam, non vestibulum lacus faucibus at. </p>
-    
-            <h3>So, what are you waiting for? Sign up for Study Nest today!</h3>
-            <button type="button" id="">Sign Up Today!</button>
+            <p>Maecenas iaculis nisi sed augue condimentum vehicula. Nunc commodo nibh in elit accumsan molestie.Donec bibendum in dui quis vehicula. Aenean ultrices pulvinar diam, non vestibulum lacus faucibus at. </p>
         </div>
         <?php
             include($_SERVER["DOCUMENT_ROOT"] . "/api/php/footer.php");
