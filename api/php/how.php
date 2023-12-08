66ea0f71b0ed327392257a4d2984119e8cfd5fbd
geodaio <powheart26@gmail.com>
2023-12-07 23:52:09 -0500

    Update how.php

--- api/php/how.php
+++ api/php/how.php
@@ -8,25 +8,27 @@
         <?php
             include($_SERVER["DOCUMENT_ROOT"] . "/api/php/nav.php");
         ?>
+    <div id="how-text">
         <h1>So, how does Study Nest Work?</h1>
         
-        <h2>Firstly, you sign up for an account.</h2>
-        <p>test</p>
+        <h2>First, complete our Study Nest quiz.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum molestie ante, non porta neque hendrerit et. Aenean molestie consequat dolor ac pharetra. Nunc placerat libero at lacus volutpat eleifend. In sapien dolor, pretium eget luctus non, hendrerit nec diam. Fusce at ante eu dolor sollicitudin convallis in at erat.</p>
         
         <h2>Once you have signed up for an account, you will receive buddies similar to yourself.</h2>
-        <p>test</p>
+        <p>Nam a bibendum sem, tempus maximus purus. Nulla vitae urna sed urna interdum congue. Fusce maximus risus arcu, et posuere purus suscipit eu. Morbi tristique sed felis porta dapibus. Aliquam sed finibus lorem, eu fringilla diam. Nulla rutrum consequat tellus, ut suscipit justo porttitor vel. Duis sed lacinia sem, quis mollis felis. Cras sem nisi, convallis at sollicitudin et, porta eu nisi. Quisque nec fermentum dolor. Sed sit amet auctor justo. </p>
         
-        <h2>More.</h2>
-        <p>test</p>
+        <h2>Add buddies, and contact them in Messages.</h2>
+        <p>Mauris sit amet urna at orci sollicitudin pharetra. Nulla sed libero elit. Nunc elit odio, ultrices sit amet risus et, viverra malesuada purus. Nulla malesuada eros at volutpat tempor. Nullam lacus nunc, scelerisque quis laoreet in, ornare ut quam. Integer tincidunt dictum orci a gravida. Aliquam erat volutpat. </p>
         
-        <h2>More.</h2>
-        <p>test</p>
+        <h2>Join study groups, to have more buddies to study with.</h2>
+        <p> Vestibulum turpis ante, ullamcorper vel felis ac, egestas accumsan nibh. Maecenas et malesuada risus. Donec eget ante eu velit fringilla placerat.</p>
         
-        <h2>More.</h2>
-        <p>test</p>
+        <h2>Complete your profile, so buddies can learn more about you.</h2>
+        <p>Morbi dictum orci justo. Cras est elit, tristique at tincidunt at, eleifend nec diam. Donec viverra leo sit amet mollis vehicula. Donec interdum euismod varius. Aliquam imperdiet tellus ultrices, venenatis nunc viverra, sodales elit. Sed eget lobortis massa, nec rhoncus felis. Aliquam erat volutpat.</p>
         
-        <h2>More.</h2>
-        <p>test</p>
+        <h2>Spend time studying to get to the top of the leaderboard!</h2>
+        <p>Donec vulputate dictum rutrum. Donec volutpat et risus a tincidunt. Curabitur efficitur ipsum quam. Duis sit amet sapien aliquet, lobortis eros eget, lobortis orci. Quisque nec nulla quis lorem facilisis finibus. Fusce viverra ullamcorper quam, id euismod nisi laoreet sit amet. Ut eget ultricies erat. Aliquam interdum sit amet ante quis consequat. Nunc eu maximus lacus. </p>
+    </div>
         <?php
             include($_SERVER["DOCUMENT_ROOT"] . "/api/php/footer.php");
             include($_SERVER["DOCUMENT_ROOT"] . "/api/php/scripts.php");
