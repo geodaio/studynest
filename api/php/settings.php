637587b9defbc0e5da10c10fc9c2f2497bf2d64d
geodaio <powheart26@gmail.com>
2023-12-08 01:46:47 -0500

    Update settings.php

--- api/php/settings.php
+++ api/php/settings.php
@@ -6,35 +6,35 @@
         <?php
             include($_SERVER["DOCUMENT_ROOT"] . "/api/php/nav.php");
         ?>
-
-        <section>
-            <h2>General</h2>
-            <h3>Change Email</h3>
-            <h3>Change Password</h3>
-            <h3>Delete Account</h3>
-        </section>
-        
-        <section>
-            <h2>Profile</h2>
-            <h3>Change Header Picture/h3>
-            <h3>Change Profile Picture/h3>
-            <h3>Edit Username</h3>
-            <h3>Edit About Me</h3>
-        </section>
-        
-        <section>
-            <h2>Notifications</h2>
-            <h3>Push Notifications</h3>
-            <h3>Muted Notifications</h3>
-        </section>
-        
-        <section>
-            <h2>Privacy</h2>
-            <h3>Blocked Accounts</h3>
-            <h3>Mark Profile as Private</h3>
-            <h3>Hide Online Status</h3>
-        </section>
-        
+        <div id="settings-block">
+            <section id="gen-settings">
+                <h2>General</h2>
+                <h3>Change Email</h3>
+                <h3>Change Password</h3>
+                <h3>Delete Account</h3>
+            </section>
+            
+            <section id="prof-settings">
+                <h2>Profile</h2>
+                <h3>Change Header Picture/h3>
+                <h3>Change Profile Picture/h3>
+                <h3>Edit Username</h3>
+                <h3>Edit About Me</h3>
+            </section>
+            
+            <section id="notif-settings">
+                <h2>Notifications</h2>
+                <h3>Push Notifications</h3>
+                <h3>Muted Notifications</h3>
+            </section>
+            
+            <section id="priv-settings">
+                <h2>Privacy</h2>
+                <h3>Blocked Accounts</h3>
+                <h3>Mark Profile as Private</h3>
+                <h3>Hide Online Status</h3>
+            </section>
+        </div>
         <?php
             include($_SERVER["DOCUMENT_ROOT"] . "/api/php/footer.php");
             include($_SERVER["DOCUMENT_ROOT"] . "/api/php/scripts.php");
