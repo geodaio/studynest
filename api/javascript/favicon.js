250ae23bd1a83a54785775bf450e9d59140f0cdb
geodaio <powheart26@gmail.com>
2023-12-08 11:49:45 -0500

    Update favicon.js

--- api/javascript/favicon.js
+++ api/javascript/favicon.js
@@ -3,6 +3,7 @@ const mode = window.matchMedia("(prefers-color-scheme: dark)");
 
  //Entry
 if (window.matchMedia("(prefers-color-scheme: dark)")) {
+   console.log("test")
    favicon.setAttribute("href", "api/images/favicon-dark.php");
  }
 else {
