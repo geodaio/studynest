26e3986f9bae97e036e9f3e6ce4398498cbb3f9f
geodaio <powheart26@gmail.com>
2023-12-08 22:21:56 -0500

    Update hub.php

--- api/php/hub.php
+++ api/php/hub.php
@@ -73,7 +73,7 @@
 		<h2 class="heading3">Looking for study buddies</h2>
 		<div class="nearyou">
 		<div class="card">
-			  <img src={"api/images/student1.jpg"} alt="John" style="width:100%">
+			  <img src="https://images.unsplash.com/photo-1568822617270-2c1579f8dfe2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D" alt="John" style="width:100%">
 			  <h3 class="studentname">Poppy Lewis</h3>
 			   <div class="cardcontent">
 			  <p class="title">@ Informatics</p>
@@ -88,7 +88,7 @@
 		</div>
 
 		<div class="card">
-			  <img src={"api/images/olivew.jpg" alt="John" style="width:100%">
+			  <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="John" style="width:100%">
 			  <h3 class="studentname">Olive Williams</h3>
 			   <div class="cardcontent">
 			  <p class="title">@ Computer Science</p>
