3f9049ca0eed5757543a06184ba707966347ae29
geodaio <powheart26@gmail.com>
2023-12-08 22:19:03 -0500

    Update hub.php

--- api/php/hub.php
+++ api/php/hub.php
@@ -118,7 +118,7 @@
 		</div>
 
 		<div class="card">
-			  <img src="" alt="John" style="width:100%">
+			  <img src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="John" style="width:100%">
 			  <h3 class="studentname">John Doe</h3>
 			  <div class="cardcontent">
 			  <p class="title">@ Cybersecurity </p>
